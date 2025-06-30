7caf8e25bccdf4817e15cffeb326e6505bde3425
Vedha Sruthika <vedhasruthikaa@gmail.com>
2025-06-30 15:15:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ import { useState } from "react";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
-
+  const [name, setName] = useState("");
   return (
     <div className={darkMode ? "container dark" : "container"}>
       <h1>Click Counter App</h1>
+      <input
+      type="text"
+      placeholder="Enter your name"
+      value={name}
+      onChange={(e) => setName(e.target.value)}
+      />
+      <p>Hello, {name ? name: "User"}</p>
       <Counter />
       <button className="btn" onClick={() => setDarkMode(!darkMode)}>
         {darkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
